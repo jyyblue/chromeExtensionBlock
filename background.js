6cc84aa688af23fd5f51366c229e0d62dee422dd
wenqiang <wenqiangliu344@gmail.com>
2024-01-24 05:54:59 +1400

    update

--- background.js
+++ background.js
@@ -1,21 +1,44 @@
 const amazon_url = "https://www.amazon.co.uk/";
 const amazon_url2 = "https://www.amazon.co.uk/ref=nav_logo";
-const ebay_url = "https://www.ebay.co.uk/";
+const amazon_url3 = "https://www.amazon.com";
+const amazon_url4 = "https://www.amazon.com/ref=nav_logo";
 
+const amazon_urls = [
+  "https://www.amazon.co.uk/",
+  "https://www.amazon.co.uk/ref=nav_logo",
+  "https://www.amazon.com",
+  "https://www.amazon.com/ref=nav_logo"
+];
+
+const ebay_url = "https://www.ebay.co.uk/";
+const ebay_url2 = "https://www.ebay.com/";
+
+const ebay_urls = [
+  "https://www.ebay.co.uk/",
+  "https://www.ebay.com/"
+];
+
+const upwork = "www.upwork.com";
 chrome.tabs.onUpdated.addListener(async (tabId,changeInfo,tab) => {
   var url = tab.url;
-  if(url == amazon_url || url == amazon_url2) {
+  if(amazon_urls.includes(url)) {
     await chrome.scripting.insertCSS({
       files: ["amazon.css"],
       target: { tabId: tabId },
     });
   }
 
-  if(url == ebay_url) {
+  if(ebay_urls.includes(url)) {
     await chrome.scripting.insertCSS({
       files: ["ebay.css"],
       target: { tabId: tabId },
     });
   }
 
+  if(url.includes(upwork)) {
+    await chrome.scripting.insertCSS({
+      files: ["upwork.css"],
+      target: { tabId: tabId },
+    });
+  }
 });
